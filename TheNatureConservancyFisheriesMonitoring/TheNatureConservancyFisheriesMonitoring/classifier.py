82ff8346a793ab80a9c9649e72d7e6e210281469
Anil Kag <anilkagak2@live.com>
2017-01-18 06:33:01 -0800

    Training possible with smaller resolution. Lets try to push first prediction model and then improve upon it.

--- TheNatureConservancyFisheriesMonitoring/TheNatureConservancyFisheriesMonitoring/classifier.py
+++ TheNatureConservancyFisheriesMonitoring/TheNatureConservancyFisheriesMonitoring/classifier.py
@@ -5,8 +5,18 @@ from scipy.misc import imresize
 from sklearn import svm, metrics
 from sklearn.model_selection import train_test_split
 import os
+import cv2
 
-newShape = (640, 480)
+'''
+Insights
+1) There are many objects in the background and it would really help the algo to just get rid of them
+2) Mostly there's water, human beings, containers, etc. These are colorful. Check if just removing them helps.
+3) Are fishes mostly black-white-silver and red in color? But there's whale which is blue
+4) Fish will be very small in the picture, other objects are relatively large and makes the pic.
+'''
+
+#newShape = (640, 480)
+newShape = (60, 40)
 
 def cleanImage(im):
     global newShape
@@ -38,9 +48,7 @@ def get_features_and_labels(data_dir):
 def saveModel(model, modelName):
     pickle.dump( model, open( modelName, "wb" ) )
 
-if __name__ == '__main__':
-    Data_Dir = 'C:\\Users\\t-anik\\Desktop\\personal\\KaggleData'
-
+def GatherTrainTestAndEvaluate(Data_Dir):
     # Process data into feature and label arrays
     X_train, X_test, y_train, y_test = get_features_and_labels(os.path.join(Data_Dir, 'train'))
 
@@ -56,3 +64,40 @@ if __name__ == '__main__':
     # Save the classifier
     saveModel(clf, "model-svc-default.bin")
 
+def experimentOnAnImage(Data_Dir):
+    print(Data_Dir)
+    label = 'ALB'
+    print(label)
+    for root, dirs, files in os.walk(os.path.join(Data_Dir, label)):
+        print(root)
+        for name in files:
+            print((os.path.join(root, name)))
+            img = imread(os.path.join(root, name)) 
+            #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+            #gray = np.float32(gray)
+            #dst = cv2.cornerHarris(gray,2,3,0.04)
+
+            #result is dilated for marking the corners, not important
+            #dst = cv2.dilate(dst,None)
+
+            # Threshold for an optimal value, it may vary depending on the image.
+            #img[dst>0.01*dst.max()]=[0,0,255]
+
+            #cv2.imshow('dst',img)
+            #cv2.imshow('img', img)
+
+            #sift = cv2.xfeatures2d.SIFT_create()
+            #kp = sift.detect(gray,None)
+            #img=cv2.drawKeypoints(gray,kp,img)
+            #cv2.imshow('sift_keypoints.jpg',img)
+            cv2.imshow('sift_keypoints.jpg',img)
+
+            cv2.waitKey()
+            #break
+
+if __name__ == '__main__':
+    Data_Dir = 'C:\\Users\\t-anik\\Desktop\\personal\\KaggleData'
+
+    GatherTrainTestAndEvaluate(Data_Dir)
+    #experimentOnAnImage(os.path.join(Data_Dir, 'train'))
