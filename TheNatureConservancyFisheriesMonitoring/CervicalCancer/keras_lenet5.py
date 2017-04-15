a5713e5b9771159445c78b4e9df5d835ed10ee07
Anil Kag <anilkagak2@live.com>
2017-04-15 05:22:12 -0700

    Tried with no data augmentation and 10 epochs log loss has reduced by 0.06

--- TheNatureConservancyFisheriesMonitoring/CervicalCancer/keras_lenet5.py
+++ TheNatureConservancyFisheriesMonitoring/CervicalCancer/keras_lenet5.py
@@ -19,7 +19,7 @@ else:
 batch_size = 32
 num_classes = len(classLabels)
 epochs = 10
-data_augmentation = True
+data_augmentation = False
 
 x_train = x_train.reshape((len(x_train), img_rows, img_cols, nchannels))
 x_test = x_test.reshape((len(x_test), img_rows, img_cols, nchannels))
